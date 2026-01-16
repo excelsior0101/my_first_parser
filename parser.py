0ffa0b83576bf5bd8a0f24a509ba1fbdcebe578b
Artur Kupava <artur30008572@gmail.com>
2026-01-16 03:57:12 +0300

    modify

--- parser.py
+++ parser.py
@@ -17,18 +17,19 @@ def fetch_onliner_catalog(url, params=None):
     
 
 def write_in_csv(mobiles):
+
     with open('mobile_catalog.csv', 'w', encoding='utf8') as ff:
         for mobile in mobiles:
             mobile_url = mobile.get('href')
             ff.write(f'{mobile.text}: {mobile_url}\n')
 
+
+def takes_mobiles(soup):
+    mobiles = soup.find_all('a', href=True, class_='catalog-form__link catalog-form__link_primary-additional catalog-form__link_base-additional catalog-form__link_font-weight_semibold catalog-form__link_nodecor')
+    print(f'Всего спарсилось {len(mobiles)} телефонов')
+    write_in_csv(mobiles=mobiles)
+
+
 URL = 'https://catalog.onliner.by/mobile'
+takes_mobiles(fetch_onliner_catalog(url=URL))
 
-soup = fetch_onliner_catalog(url=URL)
-
-mobiles = soup.find_all('a', href=True, class_='catalog-form__link catalog-form__link_primary-additional catalog-form__link_base-additional catalog-form__link_font-weight_semibold catalog-form__link_nodecor')
-print(f'Всего спарсилось {len(mobiles)} телефонов')
-
-write_in_csv(mobiles=mobiles)
-
-    
